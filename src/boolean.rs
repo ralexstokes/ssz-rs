efbbd22ce75881ba637f92aae720fbe726c2563d
Alex Stokes <r.alex.stokes@gmail.com>
2021-05-29 10:52:59 -0700

    drop type overhead on native types

--- src/boolean.rs
+++ src/boolean.rs
@@ -5,22 +5,22 @@ use crate::ser::{Serialize, SerializeError};
 #[derive(Default, Debug, PartialEq, Eq)]
 pub struct Boolean(bool);
 
-impl Serialize for Boolean {
+impl Serialize for bool {
     fn serialize(&self, buffer: &mut Vec<u8>) -> Result<usize, SerializeError> {
-        buffer.push(self.0 as u8);
+        buffer.push(*self as u8);
         Ok(1)
     }
 }
 
-impl Deserialize for Boolean {
+impl Deserialize for bool {
     fn deserialize(encoding: &[u8]) -> Result<Self, DeserializeError> {
         if encoding.len() < 1 {
             return Err(DeserializeError::InputTooShort);
         }
 
         match encoding[0] {
-            0u8 => Ok(Self(false)),
-            1u8 => Ok(Self(true)),
+            0u8 => Ok(false),
+            1u8 => Ok(true),
             _ => Err(DeserializeError::InvalidInput),
         }
     }
@@ -33,7 +33,7 @@ mod tests {
 
     #[test]
     fn encode_boolean() {
-        let tests = vec![(Default::default(), [0u8]), (Boolean(true), [1u8])];
+        let tests = vec![(Default::default(), [0u8]), (true, [1u8])];
         for (value, expected) in tests {
             let result = serialize(&value).expect("can encode");
             assert_eq!(result, expected);
@@ -42,18 +42,19 @@ mod tests {
 
     #[test]
     fn decode_boolean() {
-        let tests = vec![([0u8], Boolean(false)), ([1u8], Boolean(true))];
+        let tests = vec![([0u8], false), ([1u8], true)];
         for (bytes, expected) in tests {
-            let result = Boolean::deserialize(&bytes).expect("can encode");
+            let result = bool::deserialize(&bytes).expect("can encode");
             assert_eq!(result, expected);
         }
     }
 
     #[test]
     fn roundtrip_boolean() {
-        let value = Boolean(false);
-        let encoding = serialize(&value).expect("can encode");
-        let recovered_value = deserialize(&encoding).expect("can decode");
-        assert_eq!(value, recovered_value);
+        for value in [true, false].iter() {
+            let encoding = serialize(value).expect("can encode");
+            let recovered_value: bool = deserialize(&encoding).expect("can decode");
+            assert_eq!(*value, recovered_value);
+        }
     }
 }
