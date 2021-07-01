085f0237ba5bb01ad213dd71f71b96dca279d684
Alex Stokes <r.alex.stokes@gmail.com>
2021-06-30 17:03:35 -0700

    formatting

--- ssz/src/bitvector.rs
+++ ssz/src/bitvector.rs
@@ -8,12 +8,14 @@ use std::iter::FromIterator;
 use std::ops::{Deref, DerefMut};
 
 /// A homogenous collection of a fixed number of boolean values.
+///
 /// NOTE: a `Bitvector` of length `0` is illegal.
 ///
 /// NOTE: once `const_generics` and `const_evaluatable_checked` features stabilize,
 /// this type can use something like
 /// bitvec::array::BitArray<T, {N / 8}> where T: BitRegister, [T; {N / 8}]: BitViewSized
-/// Refer: https://stackoverflow.com/a/65462213
+///
+/// Refer: <https://stackoverflow.com/a/65462213>
 #[derive(PartialEq, Eq)]
 pub struct Bitvector<const N: usize>(BitVec);
 
