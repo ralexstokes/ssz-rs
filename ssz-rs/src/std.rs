7d636e7477d08810ef48948be38adb64994329f8
claravanstaden <Cats 4 life!>
2022-11-23 12:59:43 +0200

    Removes unnecessary license comment.

--- ssz-rs/src/std.rs
+++ ssz-rs/src/std.rs
@@ -1,11 +1,3 @@
-// Copyright 2020 Snowfork
-//
-// SPDX-License-Identifier: Apache-2.0
-//
-// Licensed under the Apache License, Version 2.0 <LICENSE or
-// http://www.apache.org/licenses/LICENSE-2.0>. This file may not be
-// copied, modified, or distributed except according to those terms.
-
 #[cfg(feature = "std")]
 pub use std::{array::TryFromSliceError, cmp::Ordering, convert::AsRef, convert::TryFrom, convert::TryInto, default::Default, fmt, fmt::Debug, fmt::Display, fmt::Formatter, iter::FromIterator, iter::Enumerate, ops::DerefMut, ops::Deref, ops::Index, ops::IndexMut, option::Option, slice::IterMut, slice::SliceIndex, vec, vec::Vec, any};
 
