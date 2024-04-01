79ed4e0ba9124289ce1084ae540a74b636a921f8
Alex Stokes <r.alex.stokes@gmail.com>
2024-04-01 13:06:46 -0600

    refactor utility fns at crate public level

--- ssz-rs/src/lib.rs
+++ ssz-rs/src/lib.rs
@@ -49,7 +49,6 @@ mod ser;
 mod serde;
 mod uint;
 mod union;
-pub mod utils;
 mod vector;
 
 mod lib {
@@ -121,9 +120,30 @@ mod exports {
         },
         ser::{Serialize, SerializeError},
         uint::U256,
-        utils::{deserialize, serialize},
         vector::Vector,
     };
+
+    /// `serialize` is a convenience function for taking a value that
+    /// implements `SimpleSerialize` and attempting to encode it to
+    /// a `Vec<u8>` according to the SSZ spec.
+    pub fn serialize<T>(value: &T) -> Result<crate::lib::Vec<u8>, SerializeError>
+    where
+        T: crate::Serializable,
+    {
+        let mut result = vec![];
+        value.serialize(&mut result)?;
+        Ok(result)
+    }
+
+    /// `deserialize` is a convenience function for taking an encoding
+    /// for some value that implements `SimpleSerialize` in a `&[u8]`
+    /// and attempting to deserialize that value from the byte representation.
+    pub fn deserialize<T>(encoding: &[u8]) -> Result<T, DeserializeError>
+    where
+        T: crate::Serializable,
+    {
+        T::deserialize(encoding)
+    }
 }
 
 pub use crate::exports::*;
